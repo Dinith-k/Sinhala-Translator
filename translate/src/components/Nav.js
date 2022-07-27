89ef73ca59a76af1d3ee42aefac27d42adf14fbf
Dinith-k <dinithkaushalya55@gmail.com>
2022-07-27 14:23:41 +0530

    updates

--- translate/src/components/Nav.js
+++ translate/src/components/Nav.js
@@ -5,6 +5,7 @@ function Nav(props) {
         <div>
             <h2>I'm a nav</h2>
             <p>Ignore me. I'm not important. I'm cool, though.</p>
+
         </div>
     );
 }
--- translate/src/pages/Dashboard.js
+++ translate/src/pages/Dashboard.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Dashboard(props) {
     console.log("Dashboard props", props);
     return (
-        <div className="App">
+        <div className="App" >
             <h2>I'm the dashboard.</h2>
-            <p>You love me &lt;3 </p>
+            <p> </p>
         </div>
     );
 }
