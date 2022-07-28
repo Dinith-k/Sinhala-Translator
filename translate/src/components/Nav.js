1d8f2416dca88fa73f27ab59c860349c2bc3900a
Dinith-k <dinithkaushalya55@gmail.com>
2022-07-28 12:54:54 +0530

    textinput updates

--- translate/src/components/Nav.js
+++ translate/src/components/Nav.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 function Nav(props) {
     return (
-        <div>
-            <h2>I'm a nav</h2>
-            <p>Ignore me. I'm not important. I'm cool, though.</p>
-
+        <div className="bord">
+<hr className="new1">
+</hr>
         </div>
     );
 }
--- translate/src/pages/Dashboard.js
+++ translate/src/pages/Dashboard.js
@@ -1,11 +1,31 @@
 import React from 'react';
-
+import '../index.css';
 function Dashboard(props) {
     console.log("Dashboard props", props);
     return (
-        <div className="App" >
-            <h2>I'm the dashboard.</h2>
-            <p> </p>
+        <div className="dash" >
+        <div class="row">
+    <div class="column">
+<div className="topset">
+    <div className="center">
+      <p class="point">English/Singlish</p>
+<input type="text" class="rounded" />
+</div>
+</div>
+
+    </div>
+    <div class="column" >
+
+
+
+    <p class="point">English</p>
+<input type="text" class="rounded" />
+<p class="point">Sinhala</p>
+<input type="text" class="rounded" />
+    </div>
+  </div>
+
+
         </div>
     );
 }
